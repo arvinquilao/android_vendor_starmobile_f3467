f5d8f76d3fce3c5ed0037c7aee1f05c8593b5f01
Arvin Quilao <arquilao@gmail.com>
2015-06-18 13:41:19 +0800

    Initial commit

---
+++ f3467-vendor.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/starmobile/f3467/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/starmobile/f3467/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/starmobile/f3467/etc,system/etc)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/starmobile/f3467/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/starmobile/f3467/xbin,system/xbin)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/starmobile/f3467/gapps/system,system)
+
+
